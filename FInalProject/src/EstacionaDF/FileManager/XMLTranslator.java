62d01a92460dbefcf19d23671bfc567dfb3e7b3a
Raphides <89037051+Raphides@users.noreply.github.com>
2022-09-04 20:05:23 -0300

    add XML translator to code
    in order to separate text content from code.

---
+++ FInalProject/src/EstacionaDF/FileManager/XMLTranslator.java
@@ -0,0 +1,43 @@
+package EstacionaDF.FileManager;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+
+public class XMLTranslator {
+    private Element rootTag;
+
+    public XMLTranslator(String languageFile) throws ParserConfigurationException, IOException, SAXException  {
+        /* BE AWARE! AN STRING PARAMATER THAT
+        GOES DIRECTLY TO CMD IS DANGEROUS!!!
+        DONT FORGET TO BRING A BETTER SOLUTION
+        TO IT LATER!!!
+        */ 
+        // prepare to receive xml
+        DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dFactory.newDocumentBuilder();
+        //get absolute path from PtBr XML file.
+        String command = "find . -type f -name " + languageFile.strip() + ".xml";
+        Process findFile = Runtime.getRuntime().exec(command);
+        Scanner filePathFinder = new Scanner(findFile.getInputStream());
+        // access xml file
+        Document doc = dBuilder.parse(new File(filePathFinder.nextLine()));
+        filePathFinder.close();
+        this.rootTag = doc.getDocumentElement();
+        
+    }
+    public Element getRootTag() {
+        return rootTag;
+    }
+
+}
