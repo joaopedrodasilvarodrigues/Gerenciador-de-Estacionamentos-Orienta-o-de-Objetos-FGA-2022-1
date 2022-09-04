309d98a9ab8488cdae3eb83eab6d14371403c34c
Raphides <89037051+Raphides@users.noreply.github.com>
2022-09-04 20:11:54 -0300

    Added xml translator class integration

--- FInalProject/src/EstacionaDF/SystemApp.java
+++ FInalProject/src/EstacionaDF/SystemApp.java
@@ -1,28 +1,36 @@
 package EstacionaDF;
 
+import java.io.IOException;
 import javax.swing.*;
-
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+import EstacionaDF.FileManager.XMLTranslator;
 import EstacionaDF.Screens.DefaultScreen;
 import EstacionaDF.Screens.HomeScreen;
 
 public class SystemApp extends JFrame{
     private DefaultScreen page;
+    private XMLTranslator textContent;
 
-    public SystemApp() {
+    public SystemApp() throws ParserConfigurationException, IOException, SAXException {
         // Essencial
         super();
+        this.textContent = new XMLTranslator("PortugueseBrasil");
         setDefaultCloseOperation(EXIT_ON_CLOSE);    
         setVisible(true);
-        setTitle("EstacionaDF");
+        setTitle(this.textContent.getRootTag().getAttribute("title"));
         setSize(500, 500);
         // setIconImage(image);
         page = new HomeScreen(this);
 
-    }
-    public DefaultScreen getPage() {
+    } public DefaultScreen getPage() {
         return page;
     } public void setPage(DefaultScreen page) {
         this.page = page;
+    } public String getTextContent(String tagName, int appearanceIndex) {
+        return this.textContent.getRootTag().getElementsByTagName(tagName).item(appearanceIndex).getTextContent();
+    } public void setTextContent(XMLTranslator textContent) {
+        this.textContent = textContent;
     }
 
 }
